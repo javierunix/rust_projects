c1dcce1ae78975ba59088ca29ae9640977bf2c95
Javier Guijarro <fjguijarromartinez@gmail.com>
2021-05-14 17:24:08 +0200

    mutable variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5; // we have to specify that the variable is mutable 
+    println!("The value of x is: {}", x );
+    
+    x = 6;
+    println!("The value of x is: {}", x );
 }
