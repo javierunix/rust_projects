00c607bb419d19683234704d832ffd586bc1ce04
Javier Guijarro <fjguijarromartinez@gmail.com>
2021-05-14 22:05:20 +0200

    finish variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,27 @@
+// rust is a statically typed language, so the it must know the type of all
+// variables when compiled
+use std::io;
+
 fn main() {
-    let mut spaces = "    ";
-    spaces = spaces.len();
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter and array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
     
-    println!("The value of spaces is: {}", spaces);
+    let element = a[index];
+
+    println!(
+        "The value of the element at index {} is: {}", index, element
+    );
 
 }
